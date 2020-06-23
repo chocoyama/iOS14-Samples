48ee59458e25a34472521511f34cda601e489a79
Takuya Yokoyama <chocoyama@hey.jp>
2020-06-23 20:38:05 +0900

    Initial Commit

---
+++ iOS14Samples/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iOS14Samples
+//
+//  Created by Takuya Yokoyama on 2020/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS14Samples/iOS14SamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14SamplesApp.swift
+//  iOS14Samples
+//
+//  Created by Takuya Yokoyama on 2020/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS14SamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
